433ce5045b1fd3c34966822905931e0070f4f9f1
Florian Schustek <schustek.florian@gmail.com>
2017-07-16 12:28:00 +0200

    fist commit

---
+++ mailServer.js
@@ -0,0 +1,121 @@
+// This is a small node server which sends emails to new users containing their temporary password
+
+// pouchdb packages
+const PouchDB = require('pouchdb')
+PouchDB.plugin(require('pouchdb-find'));
+PouchDB.plugin(require('pouchdb-authentication'));
+var remoteDb = new PouchDB('http://localhost:5984/remotedb');
+
+// "cryptography" packages
+var generator = require('generate-password');
+var md5 = require('md5');
+
+// mailgun setup 
+var api_key = 'key-551fd7590f3d18b964adcf7bbdc09868';
+var domain = 'sandbox292f29193e934d1d817a0a7d1a9f4174.mailgun.org';
+var mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+
+
+
+
+
+// this code is executed every 15 seconds and fetches the latest "authenticationEmail" objects from couchdb and then sends an email to the user with a temporary password and changes his password
+// in couchdbs _user database and in remotedb (for offline login). The "authenticationEmail" object is then deleted.
+setInterval(function(){
+     console.log("Hello"); 
+remoteDb.createIndex({
+  index: {
+    fields: ["type"]
+  }
+}).then(function () {
+    console.log("HERE");
+  return remoteDb.find({
+    selector: {type: "authenticationEmail"}
+  }).then(function (emailObject) {
+      console.log("inside succes")
+        console.log(emailObject);
+        
+        for (i in emailObject.docs){
+          
+          changePassword(emailObject.docs[i], generator.generate({length: 5, numbers: true}), function(password){      
+          var recipient = 'florian.schustek@supelec.fr';
+          var data = {
+            from: 'Vit-elec <postmaster@sandbox292f29193e934d1d817a0a7d1a9f4174.mailgun.org>',
+            to: recipient,
+            subject: 'Identifiants',
+            text: "Bonjour, un compte vous a été créé, votre identifiant est "+recipient+".\n Votre mot de passe par défaut est: "+password+", veillez à le changer"
+          };
+          mailgun.messages().send(data, function (error, body) {
+          if(error)
+          {
+          console.log(error);
+        }});
+        
+          },function(err){console.log("error2");console.log(err);})
+          remoteDb.remove(emailObject.docs[i]);
+
+        }
+        
+      }).catch(function (err) {
+          console.log("inside err")
+          console.log("error3")
+      console.log(err);
+      });
+});    
+}, 1500);
+
+
+
+
+//helper function changePassword which changes the password in the _user database but also in the remoteDb database to allow for offline login
+      function changePassword(authenticationEmail, newPassword, successCb, errorCb){
+        remoteDb.changePassword(authenticationEmail.emailAddress, newPassword, function(err, response) {
+  if (err) {
+    console.log("error4")
+    console.log(err);
+    errorCb(err);
+    if (err.name === 'not_found') {
+      // typo, or you don't have the privileges to see this user
+    } else {
+      // some other error
+    }
+  } else {
+    // response is the user update response
+    // {
+    //   "ok": true,
+    //   "id": "org.couchdb.user:spiderman",
+    //   "rev": "2-09310a62dcc7eea42bf3d4f67e8ff8c4"
+    // }
+          console.log(authenticationEmail)
+
+         // user.sync_rev = md5.createHash(newPassword || 'xy256u9i')
+          delete newPassword;
+
+                remoteDb.get(authenticationEmail.userId, function(err, user) {
+          if (err) { 
+            errorCb(err);
+            console.log(err);
+          }
+          else{
+          // handle doc
+                      user.sync_rev = md5(newPassword || 'xy256u9i')
+            remoteDb.put(user, function callback(err, result) {
+            if (!err) {
+              console.log(user);
+            }
+            else{
+              console.log("error1")
+              console.log(err);
+            }
+          });
+          }
+        });
+
+    successCb(newPassword);
+    console.log(response);
+
+    //update password hash locally
+
+  }
+})
+      }
